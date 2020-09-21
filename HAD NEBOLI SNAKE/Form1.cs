74228b42c84bb5f07b484d900d2df5504d5deba3
Michal Tichý <stunky@seznam.cz>
2020-09-21 19:22:41 +0200

    ok lepsi

--- HAD NEBOLI SNAKE/Form1.cs
+++ HAD NEBOLI SNAKE/Form1.cs
@@ -20,6 +20,7 @@ namespace HAD_NEBOLI_SNAKE
         // obtiznostnik = 0... přeskočí se 0 ticků, rychlost je maximální
         private int obtiznostnik;
         private int tmpObt;
+        private Direction tmpSmer;
        
         public Form1()
         {
@@ -107,11 +108,16 @@ namespace HAD_NEBOLI_SNAKE
             //Zkontroluje jestli je game over
             if (Settings.KonecHry || praveZapnuto)
             {
-                //Zkontroluje jestli je enter stlačen
+                //Zkontroluje jestli je enter nebo mezernik stlačen
                 if (Input.KeyPressed(Keys.Enter) || Input.KeyPressed(Keys.Space))
                 {
                     if (praveZapnuto) praveZapnuto = !praveZapnuto;
                     StartGame();
+                }
+                //esc
+                if (Input.KeyPressed(Keys.Escape))
+                {
+                    Application.Exit();
                 }
             }
             else
@@ -121,13 +127,13 @@ namespace HAD_NEBOLI_SNAKE
                     Smrt();
                 }
                 if ((Input.KeyPressed(Keys.Right) || Input.KeyPressed(Keys.D)) && Settings.směr != Direction.Left)
-                    Settings.směr = Direction.Right;
+                    tmpSmer = Direction.Right;
                 else if ((Input.KeyPressed(Keys.Left) || Input.KeyPressed(Keys.A)) && Settings.směr != Direction.Right)
-                    Settings.směr = Direction.Left;
+                    tmpSmer = Direction.Left;
                 else if ((Input.KeyPressed(Keys.Up) || Input.KeyPressed(Keys.W)) && Settings.směr != Direction.Down)
-                    Settings.směr = Direction.Up;
+                    tmpSmer = Direction.Up;
                 else if ((Input.KeyPressed(Keys.Down) || Input.KeyPressed(Keys.S)) && Settings.směr != Direction.Up)
-                    Settings.směr = Direction.Down;
+                    tmpSmer = Direction.Down;
 
                 // přeskakování ticků pro úpravu obtížnosti
                 // je to až tady aby se nemohlo stát že se přeskočí tick ve kterym se zmáčkla nějaká šipka (ignorovala by se, což nechceme žeano)
@@ -143,6 +149,7 @@ namespace HAD_NEBOLI_SNAKE
                         tmpObt = obtiznostnik;
                     }
                 }
+                Settings.směr = tmpSmer;
                 MovePlayer();
             }
 
@@ -328,8 +335,8 @@ namespace HAD_NEBOLI_SNAKE
             Settings.KonecHry = true;
 
             string gameOver =   "KONEC HRY\n\n" +
-                                "TVOJE FINÁLNÍ SKÓRE JE:  " +
-                                Settings.Score + "\n\nSNĚDL JSI:  " + Settings.PocetJidla + " JÍDLA" +
+                                "TVOJE FINÁLNÍ SKÓRE JE:  " + Settings.Score +
+                                "\n\nSNĚDL JSI:  " + Settings.PocetJidla + " JÍDLA" +
                                 "\n\nSTISKNI ENTER PRO DALŠÍ POKUS";
             lblGameOver.Text = gameOver;
 
